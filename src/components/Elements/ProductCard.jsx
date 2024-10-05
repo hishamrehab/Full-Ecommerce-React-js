e44b64644d2f4a6f447a00471dba166d2a962230
My Name <hishamrehab309@gmail.com>
2024-10-05 23:18:25 +0300

    FeaturedProducts

--- src/components/Elements/ProductCard.jsx
+++ src/components/Elements/ProductCard.jsx
@@ -1,23 +1,16 @@
+import { Link } from "react-router-dom"
 export const ProductCard = () => {
     return (
         <div className="m-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-            <a href="/" className="relative" >
+            <Link to="/" className="relative" >
                 <span className="absolute top-4 left-2 px-2 bg-orange-500 bg-opacity-90 text-white rounded">Best Seller</span>
-                <img className="rounded-t-lg w-full h-64" src="" alt="" />
-            </a>
+                <img className="rounded-t-lg w-full h-64" src="/assets/images/10001.avif" alt="" />
+            </Link>
             <div className="p-5">
-                <a href="/">
+                <Link to="/">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">The Complete Guide to Backend Development</h5>
-                </a>
+                </Link>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error unde quisquam magni vel eligendi nam.</p>
-
-                <div className="flex items-center my-2">
-                    <i className="text-lg bi bi-star-fill text-yellow-500 mr-1"></i>
-                    <i className="text-lg bi bi-star-fill text-yellow-500 mr-1"></i>
-                    <i className="text-lg bi bi-star-fill text-yellow-500 mr-1"></i>
-                    <i className="text-lg bi bi-star-fill text-yellow-500 mr-1"></i>
-                    <i className="text-lg bi bi-star text-yellow-500 mr-1"></i>
-                </div>
 
                 <p className="flex justify-between items-center">
                     <span className="text-2xl dark:text-gray-200">
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,9 +1,10 @@
 import { Hero } from "./components/Hero"
-
+import { FeaturedProducts } from "./components/FeaturedProducts"
 export const HomePage = () => {
     return (
         <main>
             <Hero />
+            <FeaturedProducts />
         </main>
     )
 }
