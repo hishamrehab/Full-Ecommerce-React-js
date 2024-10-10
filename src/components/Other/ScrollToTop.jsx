1bd1913c29b524827f83b60ef1dfec3d5a506fb8
My Name <hishamrehab309@gmail.com>
2024-10-10 04:07:15 +0300

    Scroll to top

--- src/components/Other/ScrollToTop.jsx
+++ src/components/Other/ScrollToTop.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom"
 
-const ScrollToTop = () => {
+export const ScrollToTop = () => {
+    const { pathname } = useLocation();
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [pathname]);
+
     return (
-        <div>
-            ScrollToTop
-        </div>
+        null
     )
 }
 
-export default ScrollToTop
+
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,5 @@
 export { Header } from "./Layouts/Header"
 export { Footer } from "./Layouts/Footer"
 export { ProductCard } from "./Elements/ProductCard"
+
+export { ScrollToTop } from "./Other/ScrollToTop"
